fab6efe7915df74be52b2a0a0e4562bb907af82a
Rolf Bjarne Kvinge <rolf@xamarin.com>
2015-10-13 13:28:21 +0200

    Add test case.

--- bug34794/ViewController.m
+++ bug34794/ViewController.m
@@ -17,11 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    UILabel *label = [[UILabel alloc] init];
+    label.text = @"I should be landscape only, sir.";
+    label.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    label.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(NSUInteger) supportsInterfaceOrientations {
+    return UIInterfaceOrientationMaskLandscape;
 }
 
 @end
